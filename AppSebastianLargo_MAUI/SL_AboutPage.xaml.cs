51f5238103a05a8c60e7451e0b9016318964cfc3
mango2309 <sebastian.largo@udla.edu.ec>
2024-05-29 11:21:13 -0500

    Controlador del button clicked

---
+++ AppSebastianLargo_MAUI/SL_AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+namespace AppSebastianLargo_MAUI;
+
+public partial class AboutPage_SL : ContentPage
+{
+	public AboutPage_SL()
+	{
+		InitializeComponent();
+	}
+
+    private void LearnMore_Clicked(object sender, EventArgs e)
+    {
+
+    }
+}
